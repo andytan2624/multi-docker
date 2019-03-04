0f9807237212aff6609c7999328479b918c0dde1
andytan2624 <andy.tan2624@gmail.com>
2019-03-05 08:59:39 +1100

    Tangy 2 added memory allocation

--- client/src/App.js
+++ client/src/App.js
@@ -12,7 +12,7 @@ class App extends Component {
         <div className="App">
           <header className="App-header">
             <img src={logo} className="App-logo" alt="logo" />
-            <h1 className="Apptitle">Welcome to react</h1>
+            <h1 className="Apptitle">Fib Calculator</h1>
             <Link to="/">Home</Link>
             <Link to="/otherpage">Other page</Link>
           </header>
